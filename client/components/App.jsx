ae52c14454f25b3a7d620cd251e03541a8f9158c
Jessica Yang <jessicaawhy@gmail.com>
2021-12-07 17:42:32 -0800

    Add routing with hash router

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import Home from './Home/index';
+import Products from './Products/index';
+import Profile from './Profile/index';
+import Lifestyle from './Lifestyle/index';
+import NotFound from './NotFound/index';
 
 const App = () => (
-  <h1>Hello, world!</h1>
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="products" element={<Products />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="lifestyle" element={<Lifestyle />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
 const app = require('./app');
 
 app.listen(3000, () => console.log('now listening on port 3000'));
-
