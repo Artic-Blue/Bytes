831e5bc5f6c806323923cf237b062bb772fed278
Jessica Yang <jessicaawhy@gmail.com>
2021-12-10 14:56:37 -0800

    Set up server skeleton and add sample routes

--- db/index.js
+++ db/index.js
@@ -1,18 +1,11 @@
 const { Pool } = require('pg');
 
-const credentials = {
-  user: 'noahlehman',
-  host: 'localhost',
-  database: 'bytes',
-  password: '',
-  port: 5432,
-};
-
-const pool = new Pool(credentials);
-// async function query() {
-//   const pool = new Pool(credentials);
-//   await pool.query('');
-//   await pool.end();
-// }
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
 
 module.exports = pool;
