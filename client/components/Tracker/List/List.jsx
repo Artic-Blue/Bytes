cabf7ccbd69b49be7712cc7cbdc3e69c5da213c4
Yoseob Kim <ykim9611@gmail.com>
2021-12-10 10:35:33 -0800

    Display only 4 listitems initially. Load 4 more upon each click of the 'load more' button.

--- client/components/Tracker/List/List.jsx
+++ client/components/Tracker/List/List.jsx
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import dummyData from './dummy_data';
+import ListItem from './ListItem';
 
 const List = () => {
   const [thoughtsList, updateThoughtsList] = useState([]);
+  const [renderCount, updateRenderCount] = useState(4);
+
+  useEffect(() => {
+    updateThoughtsList(dummyData);
+  }, []);
+
   return (
-    <h2>List</h2>
+    <>
+      <h2>List</h2>
+      {thoughtsList.filter((item) => thoughtsList.indexOf(item) < renderCount).map((item) => <ListItem item={item} />)}
+      <button onClick={()=>updateRenderCount(renderCount + 4)}>Load More</button>
+    </>
   );
 };
 
---
+++ client/components/Tracker/List/dummy_data.js
@@ -0,0 +1,34 @@
+const dummyData = [
+  {
+    tracker_id: '1', user_id: '2', thought: 'I ate a carrot it was good', feeling_id: '4', track_date: '2021-12-05',
+  },
+  {
+    tracker_id: '2', user_id: '2', thought: 'I ate a banana and it was terrible', feeling_id: '1', track_date: '2021-12-06',
+  },
+  {
+    tracker_id: '3', user_id: '2', thought: 'I lost my shoe', feeling_id: '1', track_date: '2021-track_12-07',
+  },
+  {
+    tracker_id: '4', user_id: '2', thought: 'I fasted today', feeling_id: '3', track_date: '2021-track_12-08',
+  },
+  {
+    tracker_id: '5', user_id: '2', thought: 'I was coding all day', feeling_id: '5', track_date: '2021-12-09',
+  },
+  {
+    tracker_id: '6', user_id: '2', thought: 'I fell into a deep deep slumber', feeling_id: '4', track_date: '2021-12-10',
+  },
+  {
+    tracker_id: '7', user_id: '2', thought: 'I made new friends', feeling_id: '1', track_date: '2track_021-12-11',
+  },
+  {
+    tracker_id: '8', user_id: '2', thought: 'I lost my pen', feeling_id: '1', track_date: '2021-track_12-12',
+  },
+  {
+    tracker_id: '9', user_id: '2', thought: 'I forgot to eat today', feeling_id: '3', track_date: '2021-track_12-13',
+  },
+  {
+    tracker_id: '10', user_id: '2', thought: 'I was running all day', feeling_id: '5', track_date: '2021-12-14',
+  },
+];
+
+export default dummyData;
