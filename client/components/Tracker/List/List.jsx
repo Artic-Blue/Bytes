5e6d91c5f751b2268f134145bae68b16c3965d2d
Yoseob Kim <ykim9611@gmail.com>
2021-12-11 10:01:33 -0800

    Implemented list filtering feature for MVP.

--- client/components/Tracker/List/List.jsx
+++ client/components/Tracker/List/List.jsx
@@ -16,39 +16,12 @@ const List = () => {
   const setRenderCountHandler = () => {
     if (renderCount <= thoughtsList.length) {
       setRenderCount((prevRenderCount) => prevRenderCount + 4);
-      console.log('Render Count: ', renderCount);
-      console.log('Pre Render Amount: ', renderAmount);
     } else if (renderCount >= thoughtsList.length) {
       setRenderAmount((prevRenderAmount) => prevRenderAmount + 20);
-      console.log('Render Amount: ', renderAmount);
       setThoughtsList(thoughtsList.concat(dummyData.slice(renderAmount, renderAmount + 20)));
       setRenderCount((prevRenderCount) => prevRenderCount + 4);
-      console.log('Updated Render Count: ', renderCount);
-      console.log('Actual Array Of Data: ', thoughtsList);
-      console.log('Updated Render Amount: ', renderAmount);
     }
   };
-
-  // Bugs* whenlist reaches 40 render amount needs to be updated before list can be updated
-  // When renderCount reaches 40 renderAmount is alreadyat 40 and jumps to 60 then the list reloads and there is no data rendered to page
-
-  /*
-
-  const setRenderCountHandler = () => {
-    if (renderCount + 1 < thoughtsList.length) {~
-      setRenderCount(renderCount + 4);
-      console.log('Render Count: ', renderCount)
-    } else if (renderCount + 1 > thoughtsList.length) {
-      setRenderAmount(renderAmount + 20);
-      console.log('Render Amount: ', renderAmount)
-      setThoughtsList(dummyData.splice(0, renderAmount));
-      setRenderCount(renderCount + 4);
-      console.log('Updated Render Count: ', renderCount)
-
-    }
-  };
-
-  */
 
   return (
     <>
