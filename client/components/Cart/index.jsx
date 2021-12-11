39e6b195701d7f4855997a6dc519d7a64e6873ff
Abe Froman <abefroman@Abes-MacBook-Pro.local>
2021-12-11 11:51:47 -0800

    The cart page shows a list of three dummy products. Has a 'check out' button with a bare listener.

--- client/components/Cart/index.jsx
+++ client/components/Cart/index.jsx
@@ -1,7 +1,32 @@
+/* eslint-disable no-alert */
 import React from 'react';
 
-const Cart = () => (
-  <h2>Cart</h2>
-);
+const Cart = () => {
+  const cartItems = [
+    'Milk',
+    'Strawberries',
+    'Avocados',
+  ];
+  const cartItemsAsListElements = cartItems.map((item) => <li>{item}</li>);
+
+  function checkOut(e) {
+    e.preventDefault();
+    alert('Stay tuned! Future versions of this app will allow customers to check out and pay for their food items.');
+  }
+
+  return (
+    <div>
+      <h2>Cart</h2>
+      <h2>These are the items in your cart-- </h2>
+      <ul>
+        {cartItemsAsListElements}
+      </ul>
+      <form onSubmit={checkOut}>
+        <button type="submit">Check Out and Pay</button>
+      </form>
+
+    </div>
+  );
+};
 
 export default Cart;
