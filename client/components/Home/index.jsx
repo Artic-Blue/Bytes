c680723087696afff011012ee680fea18dd0750d
Yoseob Kim <ykim9611@gmail.com>
2021-12-14 21:53:48 -0800

    Styled 2 buttons onto home screen (positioning still needs work)

--- client/components/Home/index.jsx
+++ client/components/Home/index.jsx
@@ -4,30 +4,97 @@ import styled from 'styled-components';
 
 const Home = () => (
   <>
-    <Title>Home</Title>
+    <Title>
+      <LogoDiv>
+        <img src="../images/best-eats-inc.png" alt="logo" />
+      </LogoDiv>
+      <img src="../images/cornucopia.jpg" alt="bountiful bounty of food items" />
+    </Title>
     <Links>
-      <Link to="/shop">Shop</Link>
-      <Link to="/tracker">Tracker</Link>
-      <Link to="/shop/farmers">Meet Our Farmers</Link>
+      <StyledLink to="/shop/meals">
+        <ButtonToPage>
+          <img src="../images/meals.jpg" alt="" />
+          <CenterText>
+            Meals
+          </CenterText>
+        </ButtonToPage>
+      </StyledLink>
+      {/* <StyledLink to="/shop/products">
+        <ButtonToPage>
+          <img src="../images/groceries.jpeg" alt="" />
+          <CenterText>
+            Shop Groceries
+          </CenterText>
+        </ButtonToPage>
+      </StyledLink> */}
+      <StyledLink to="/tracker">
+        <ButtonToPage>
+          <img src="../images/lifestyle.jpg" alt="" />
+          <CenterText>
+            Lifestyle
+          </CenterText>
+        </ButtonToPage>
+      </StyledLink>
     </Links>
   </>
 );
 
-const Title = styled.h1`
-  font-size: 20vh;
+const LogoDiv = styled.div`
+  display: flex;
+  position: absolute;
+  bottom: 7%;
+  right: 5%;
+  img{
+    max-height: 25vh;
+    max-width: auto;  }
+`;
+
+const Title = styled.div`
+  display: flex;
+  position: relative;
+  justify-content: center;
+  text-align: center;
+  overflow: hidden;
+
+  img {
+    max-height: 70vh;
+    max-width: auto;
+  }
+`;
+
+const Links = styled.div`
   display: flex;
   width: 50vw;
   height: 30vh;
   position: relative;
-  align-items: flex-end;
+  justify-content: space-between;
+  align-items: center;
+  overflow: hidden;
 `;
 
-const Links = styled.div`
+const ButtonToPage = styled.div`
 display: flex;
-width: 95vw;
-height: 50vh;
-position: relative;
-justify-content: space-around;
-align-items: flex-end;
+text-align: center;
+overflow: hidden;
+
+img {
+  max-width: 30vw;
+  max-height: 26vh;
+}
 `;
+
+const StyledLink = styled(Link)`
+  color: white;
+  font-weight: 900;
+  font-size: 40px;
+  text-decoration: none;
+`;
+
+const CenterText = styled.div`
+  display: flex;
+  position: relative;
+  top: 110px;
+  right: 50%;
+`;
+
 export default Home;
